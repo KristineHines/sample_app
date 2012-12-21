0ea339c8d7d92f6facb8b94905202c3156855dce
Kristine Hines <hines.kristine@gmail.com>
2012-12-20 23:16:23 -0600

    Email validation mixed case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,6 +65,16 @@ require 'spec_helper'
     end
   end
 
+    describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " "}
     it { should_not be_valid}
