a3cc8b34a3880b5c8312ff3cf8baded4b37d5cb8
Kristine Hines <hines.kristine@gmail.com>
2012-12-22 16:09:42 -0600

    Added Gravitar to the user page,changed the message for blank password block, add tests for after subimission and after saving the user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,5 @@ class User < ActiveRecord::Base
   					uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-
 end
+
